10509182106db144ff1e21eecf72f6b4a513dd49
Arnab Sen <arnb.sen@hotmail.com>
2017-11-28 15:45:57 +0530

    Initial Commit

--- LabC++/adjList.cpp
+++ LabC++/adjList.cpp
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <iostream>
 using namespace std;
+//Class for Nodes of a vetex in adjacent list (vertex,weight)
 class Node{
     int v;
     int w;
@@ -21,6 +22,7 @@ class Node{
         next = nullptr;
     }
 };
+//Class for Adjacency list contains vertex and pointer to adjacent nodes
 class AdjNode{
     int u;
     AdjNode* next;
@@ -32,6 +34,7 @@ class AdjNode{
         adj = nullptr;
     }
 };
+//Class to maintian the Adjacent List
 class AdjList{
     AdjNode* head;
 public:
@@ -61,27 +64,57 @@ public:
             while(temp1->next!=nullptr){
                 if(temp1->v==v){
                     break;
+                    return;
                 }
                 temp1 = temp1->next;
             }
             temp1->next = new Node(v,w);
         }
     }
-    
+    int returnWeight(int u, int v){
+        AdjNode* temp = head;
+        int w = -1;
+        while (temp->u!=u&&temp->next!=nullptr) {
+            temp = temp->next;
+        }
+        if (temp->adj==nullptr) {
+            w = -1;
+        }else{
+            Node* temp1 = temp->adj;
+            while(temp1!=nullptr){
+                if(temp1->v==v){
+                    w = temp1->w;
+                    break;
+                }
+                temp1 = temp1->next;
+            }
+            
+        }
+        return w;
+    }
     
 };
 int main(){
     AdjList adj;
+    
+    
+    //Add vertices of the Graph
     adj.addNewNode(1);
     adj.addNewNode(2);
     adj.addNewNode(3);
     adj.addNewNode(4);
+    
+    
+    //Add the adjacent vertices and weight connecting them
     adj.addNewAdj(1, 2, 10);
     adj.addNewAdj(1, 3, 12);
     adj.addNewAdj(1, 4, 13);
     adj.addNewAdj(2, 3, 11);
     adj.addNewAdj(2, 1, 19);
     adj.addNewAdj(1, 3, 12);
-
+    
+    //Returning the weigth of the vertex
+    cout<<adj.returnWeight(2, 3)<<endl;
+    cout<<adj.returnWeight(1, 3)<<endl;
 
 }
